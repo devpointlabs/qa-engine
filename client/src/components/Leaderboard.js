ddcf97e73ad5978946ec88b28628f7c0f557225f
Layne Rasmussen <layrasm@gmail.com>
2020-10-28 12:46:46 -0600

    leaderboard styling

--- client/src/components/Leaderboard.js
+++ client/src/components/Leaderboard.js
@@ -21,7 +21,7 @@ const Leaderboard = () => {
     return upvotes.map((u) => (
       <Card.Group>
         <Card fluid color='red' header='Option 1'>
-        <h1>Leaderboard</h1>
+        
         
         <h4>User Name: {u.first_name} {u.last_name}</h4>
         <h5>Upvotes: {u.total_upvotes}</h5>
@@ -39,7 +39,7 @@ const Leaderboard = () => {
 
   return (
     <div>
-      
+        <h1>Leaderboard</h1>
         {renderLeaderboard()}
     </div>
   )
